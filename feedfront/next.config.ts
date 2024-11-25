1259d5898d5fb992dc310f2c386a29d6dfb28880
loubruness <94389521+loubruness@users.noreply.github.com>
2024-11-25 10:52:09 +0100

    pitit oubli

--- feedfront/next.config.ts
+++ feedfront/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  env: {
+    API_HOST: 'http://localhost:3000',
+  },
 };
 
 export default nextConfig;
