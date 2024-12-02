6dd34563d171c70d684f0adda5d3c04369c8cd2a
{Arthur Billebaut} <{arthur.billebaut@efrei.net}>
2024-12-02 12:12:57 +0100

    default form status db

--- internal-db-init.sql
+++ internal-db-init.sql
@@ -123,7 +123,7 @@ INSERT INTO admin (Id) VALUES (13);
 INSERT INTO admin (Id) VALUES (14);
 INSERT INTO admin (Id) VALUES (15);
 
-INSERT INTO forms (course_name, end_date, Id_admin) VALUES ('Default', '2021-12-31', 11);
+INSERT INTO forms (course_name, end_date, Id_admin, status) VALUES ('Default', '2021-12-31', 11, 'default');
 
 INSERT INTO fields (Id_form, title, question) VALUES (1, 'Satisfaction', 'Overall, I am satisfied with the lessons with this teacher');
 INSERT INTO fields (Id_form, title, question) VALUES (1, 'Availability', 'The teacher is available and listens to us');
