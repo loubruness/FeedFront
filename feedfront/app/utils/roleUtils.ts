9f53621e94ca9b1716f3af76f466525f25348865
paulinedavid <pauline.david@efrei.net>
2024-12-03 15:01:17 +0100

    route verifyToken done

--- feedfront/app/utils/roleUtils.ts
+++ feedfront/app/utils/roleUtils.ts
@@ -2,6 +2,6 @@ export const getUserRole = (): "admin" | "student" | "teacher" => {
     // logic to determine user role (e.g., from localStorage, API, or context)
     // const user = JSON.parse(localStorage.getItem("user") || "{}");
     // return user.role || "student";
-    return "teacher";
+    return "admin";
   };
   
