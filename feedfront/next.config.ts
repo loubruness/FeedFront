f2c1aed0853abc9e2d9cda88d75f73b2cde7b6a6
Hugo PANEL <hugopanel@gmail.com>
2024-12-11 13:40:08 +0100

    Added new docker config

--- feedfront/next.config.ts
+++ feedfront/next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  env: {
-    NEXT_PUBLIC_BACK_ADDRESS: `${process.env.NEXT_PUBLIC_BACK_ADDRESS}`,
-  },
 };
 
 export default nextConfig;
